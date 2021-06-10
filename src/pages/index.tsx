5c52f5a709778208200e4a448329e1cbfb29c3bc
edrajao <4ucode@gmail.com>
2021-06-09 21:47:10 -0300

    Seguindo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,51 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import React from "react";
 
-export default function Home() {
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
-    <Flex h="100vh" alignItems="center" justifyContent="center">
-      <h1>DashGo</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input
+            label="E-mail"
+            name="email"
+            type="email"
+            focusBorderColor="pink.500"
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{
+              bgColor: "gray.900",
+            }}
+            size="lg"
+          />
+          <Input
+            label="Senha"
+            name="password"
+            type="password"
+            focusBorderColor="pink.500"
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{
+              bgColor: "gray.900",
+            }}
+            size="lg"
+          />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
     </Flex>
   );
 }
